62c2691d2101104f36b917f56122dc744ba038ca
ORIONN0111 <poppinpoppin111@gmail.com>
2022-03-06 17:09:58 +0900

    破壊可能objを作った

--- Assets/Stages/Scripts/Break.cs
+++ Assets/Stages/Scripts/Break.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Break : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int hp;
 
     // Update is called once per frame
     void Update()
     {
-        
+        // this.GetComponent<ParticleSystem>().Play(); test
+    }
+    public void DamageWall(int loss) {
+        hp -= loss;
+
+        this.GetComponent<ParticleSystem>().Play();
+
+        if (hp <= 0) {
+            gameObject.SetActive(false);
+        }
     }
 }
