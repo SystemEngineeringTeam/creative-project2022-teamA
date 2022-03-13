5387b88befe8f821c364fbadd664ae2ce540640a
hyo <x19053xx@aitech.ac.jp>
2022-03-13 20:29:33 +0900

    ゲームの動作を行うシーンを作成

---
+++ Assets/GameManages/Scripts/ActionManager.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Cinemachine;
+public class ActionManager : MonoBehaviour
+{
+    [Header("プレイヤー関連")]
+    public GameObject  playerPlefab;
+
+    [Header("カメラ設定")]
+    public Transform CameraTransform;
+    public CinemachineVirtualCamera virtualCamera;
+
+    [Header("ステージ関連")]
+    public string activeSceneName;
+    string beforeSceneName;
+    GameObject playerObject;
+    public List<Scene> LoadedScenes=new List<Scene>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (playerPlefab==null)
+        {
+            playerObject = new GameObject();
+            playerObject.AddComponent<ViewingMode>();
+            virtualCamera.GetComponent<CinemachineConfiner>().enabled=false;
+            
+        }else{
+            playerObject=Instantiate(playerPlefab);
+        }
+        virtualCamera.Follow=playerObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(string.Compare(beforeSceneName,activeSceneName)!=0){
+            beforeSceneName=activeSceneName;
+            TransitionScene(activeSceneName,Vector3.zero);
+        }
+    }
+
+    public void ChangePlayerfromPlefab(){
+        ChangePlayerfromPlefab(playerPlefab);
+    }
+    public void ChangePlayerfromPlefab(GameObject plafab){
+        playerObject=Instantiate(plafab);
+    }
+
+    #region SceneFunctions
+    public void TransitionScene(string sceneName, Vector3 playerPosition){
+        StartCoroutine(CoroutineTsScene(sceneName,playerPosition));
+    }
+
+    IEnumerator CoroutineTsScene(string sceneName, Vector3 playerPos){
+        playerObject.SetActive(false);
+        playerObject.transform.position=playerPos;
+        foreach (Scene scene in LoadedScenes)
+        {
+            yield return SceneManager.UnloadSceneAsync(scene);
+        }
+        LoadedScenes.Clear();
+        yield return SceneManager.LoadSceneAsync(sceneName,LoadSceneMode.Additive);
+        Scene loadedScene = SceneManager.GetSceneByName(sceneName);
+        // Debug.Log(SceneManager.GetActiveScene().name);
+        GameObject camColliderObj = GameObject.FindGameObjectWithTag("CameraCollider");
+        if (camColliderObj==null)
+        {
+            virtualCamera.GetComponent<CinemachineConfiner>().enabled=false;
+        }else{
+            if(camColliderObj.GetComponent<Collider2D>()){
+                virtualCamera.GetComponent<CinemachineConfiner>().m_BoundingShape2D = camColliderObj.GetComponent<Collider2D>();
+            virtualCamera.GetComponent<CinemachineConfiner>().enabled=true;
+            }
+        }
+        foreach (GameObject camObject in GameObject.FindGameObjectsWithTag("MainCamera"))
+        {
+            if(string.Compare(camObject.scene.name,sceneName)==0){
+                Destroy(camObject);
+            }
+        }
+        LoadedScenes.Add(loadedScene);
+        playerObject.SetActive(true);
+    }
+
+    public void LoadAdditionalScene(string sceneName){
+        StartCoroutine(CoroutineAddScene(sceneName));
+    }
+    public void UnloadAdditionalScene(string sceneName){
+        StartCoroutine(CoroutineRemoveScene(sceneName));
+    }
+    IEnumerator CoroutineAddScene(string sceneName){
+        yield return SceneManager.LoadSceneAsync(sceneName,LoadSceneMode.Additive);
+        LoadedScenes.Add(SceneManager.GetSceneByName(sceneName));
+    }
+    IEnumerator CoroutineRemoveScene(string sceneName){
+        Scene loadedScene = SceneManager.GetSceneByName(sceneName);
+        yield return SceneManager.LoadSceneAsync(sceneName,LoadSceneMode.Additive);
+        LoadedScenes.RemoveAll((s)=>loadedScene.Equals(s));
+        
+    }
+
+    #endregion
+
+}
