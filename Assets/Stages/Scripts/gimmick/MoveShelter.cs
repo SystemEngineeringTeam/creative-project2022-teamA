8630863ebf62916deb7b975ed59316c979ba7db9
ORIONN0111 <poppinpoppin111@gmail.com>
2022-03-14 18:39:34 +0900

    エレベーター実装

--- Assets/Stages/Scripts/gimmick/MoveShelter.cs
+++ Assets/Stages/Scripts/gimmick/MoveShelter.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveShelter : MonoBehaviour
 {
-    public bool OnMoveShelter;
+    private bool OnMoveShelter;
     private void Start() {
         OnMoveShelter=false;
     }
