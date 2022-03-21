fc4d328f7743608ab28e394e179f7ba859f883ad
わだたける <wdtkru@icloud.com>
2022-03-22 01:57:03 +0900

    小ジャンプ・大ジャンプの実装、FixedUpdateとUpdateの区別化

--- Assets/Actions/Player/Scripts/PlayerController.cs
+++ Assets/Actions/Player/Scripts/PlayerController.cs
@@ -16,10 +16,10 @@ public class PlayerController : MonoBehaviour
     // インスペクターで変更可能
     public GroundCheck ground; //接地判定
 	public WallJump wall; //壁ジャンプ判定
-    public float jumpForce = 1000f;       // ジャンプ時に加える力
+    public float jumpForce = 700f;       // ジャンプ時に加える力
 	public float runSpeed = 10.0f;       // 走っている間の速度
 	public float runThreshold = 2.2f;   // 速度切り替え判定のための閾値
-
+	public float timer = 0;
 
     private Animator anim = null;
     private Rigidbody2D rb = null;
@@ -30,6 +30,11 @@ public class PlayerController : MonoBehaviour
 	private string prevState;            // 前の状態を保存
     private bool isGround = true;        // 地面と接地しているか管理するフラグ
 	private bool isWall = true;        // 壁と接しているか管理するフラグ
+	private bool isJump = false;      // ジャンプしている最中か管理するフラグ
+	public bool jumpKeyDown = false; //ジャンプボタンを押した瞬間を管理
+	private bool jumpKey = false; //ジャンプボタンを押してる間を管理
+	private bool jumpKeyUp = false; //ジャンプボタンを離した瞬間を管理
+
 	private int tmp = 0;
 
     // Start is called before the first frame update
@@ -40,8 +45,26 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update(){
+    // Update
+	void Update(){
+		// keyの押下はUpdateで判定(FixedUpdateでは絶対に行ってはいけない。)
+
+		if(Input.GetKeyDown(KeyCode.Space)){
+			jumpKeyDown = true;
+		}else if(Input.GetKey(KeyCode.Space)){
+			jumpKey = true;
+		}else if(Input.GetKeyUp(KeyCode.Space)){
+			jumpKeyUp = true;
+		}
+
+		if(ground.EnterGround()){
+			timer = 0.0f;
+			jumpKeyDown = false;
+			jumpKey = false;
+			jumpKeyUp = false;
+		}
+	}
+    void FixedUpdate(){
 		GetInputKey ();
         ChangeState();
 		ChangeAnimation();
@@ -107,35 +130,30 @@ public class PlayerController : MonoBehaviour
 					anim.SetBool ("walk_flag", false);
 					anim.SetBool ("jump_up_flag", true);
 					anim.SetBool ("jump_down_flag", false);
-					Debug.Log("a");
 					break;
 				case "FALL":
 					anim.SetBool ("run_flag", false);
 					anim.SetBool ("walk_flag", false);
 					anim.SetBool ("jump_up_flag", false);
 					anim.SetBool ("jump_down_flag", true);
-					Debug.Log("i");
 					break;
 				case "RUN":
 					anim.SetBool ("run_flag", true);
 					anim.SetBool ("walk_flag", false);
 					anim.SetBool ("jump_up_flag", false);
 					anim.SetBool ("jump_down_flag", false);
-					Debug.Log("u");
 					break;
 				case "WALK":
 					anim.SetBool ("run_flag", false);
 					anim.SetBool ("walk_flag", true);
 					anim.SetBool ("jump_up_flag", false);
 					anim.SetBool ("jump_down_flag", false);
-					Debug.Log("e");
 					break;
 				default:
 					anim.SetBool ("run_flag", false);
 					anim.SetBool ("walk_flag", false);
 					anim.SetBool ("jump_up_flag", false);
 					anim.SetBool ("jump_down_flag", false);
-					Debug.Log("o");
 					break;
 			}
 			// 状態の変更を判定するために状態を保存しておく
@@ -145,26 +163,18 @@ public class PlayerController : MonoBehaviour
 
     void Move(){
 		// 接地してる時にSpaceキー押下でジャンプ
-		float speedX = Mathf.Abs (this.rb.velocity.x);
-
 		if(isGround){
-			if (Input.GetKeyDown(KeyCode.Space)) {
-
-				// ボタンの押す長さでジャンプの高さを変化させたい
-				rb.velocity = new Vector2(0,0);
+			if (jumpKeyDown) {
+				timer = 0.0f;
+				rb.velocity = new Vector2(rb.velocity.x,0);
 				rb.AddForce (transform.up * this.jumpForce);
 
 				isGround = false;
 				isWall = false;
-				// Debug.Log("ground");
+				jumpKeyDown = false;
 			}
 		}else if(isWall){
-			// ズサーを追加したい
-			// 下に力が働いていて、正面の方向キーを入力している時にだけズサーが出るように
-
-			// if(Input.GetKeyDown(KeyCode.))
-
-			if(Input.GetKeyDown(KeyCode.Space)){
+			if(jumpKeyDown){
 				rb.velocity = new Vector2(0,0);
 				transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
 				// 壁ジャンプで向きを反転
@@ -173,9 +183,16 @@ public class PlayerController : MonoBehaviour
 				// 斜め上方向にジャンプ
 				// 進みたい方向キーを入力しながら壁ジャンプすると、壁ジャンプの飛距離が増加
 				
+				jumpKeyDown = false;
 				isWall = false;
 				// Debug.Log("wall");
 			}
+		}
+		
+		// 長押しジャンプ処理
+		if(jumpKey && !jumpKeyUp && timer < 0.3f && rb.velocity.y > 0){
+			rb.AddForce (transform.up * this.jumpForce * Time.deltaTime * 2);
+			timer += Time.deltaTime;
 		}
 		
 		// 左右の移動
